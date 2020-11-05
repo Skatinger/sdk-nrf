58fa32dd8b2e0b33453c535c03fc2154b0f36fe3
Skatinger <alex.nyffenegger@outlook.com>
2020-11-05 14:58:43 +0100

    cleanup, documentation

--- applications/asset_tracker/src/coap_cloud/src/coap_cloud.c
+++ applications/asset_tracker/src/coap_cloud/src/coap_cloud.c
@@ -21,12 +21,10 @@ static const char * const obs_path[] = { "led", NULL };
 static const char * const test_path[] = { "testing", NULL };
 
 // TODO add to config files
-// #define CONFIG_COAP_RESOURCE "other/block"// "obs"
 #define CONFIG_COAP_SERVER_HOSTNAME "83.150.54.152"
 
 static struct coap_client client;
-// static struct sockaddr_storage broker;
-static struct sockaddr_in broker; // TODO use ipv6 eventually
+static struct sockaddr_in broker;
 static int sock;
 static int poll_sock;
 static u16_t next_token;
@@ -34,13 +32,13 @@ static u16_t next_token;
 static u8_t coap_buf[APP_COAP_MAX_MSG_LEN];
 
 static struct cloud_backend *coap_cloud_backend;
-
+// allow to stop the polling
 static atomic_t disconnect_requested;
-static atomic_t connection_poll_active;
-
 static K_SEM_DEFINE(connection_poll_sem, 0, 1);
 
-
+/* send a reply if an observation has been received
+ * takes the message id, token and the token length of the CON message
+ * for which this acc should be sent */
 static int send_obs_reply_ack(uint16_t id, uint8_t *token, uint8_t tkl)
 {
 	struct coap_packet request;
@@ -65,6 +63,9 @@ end:
 	return r;
 }
 
+/* processes a simple coap request, skips if nothing received,
+ * as we do not need confirmation from server, we send data even if not reachable,
+ * in case the server comes online again */
 static int process_simple_coap_reply(void)
 {
 	struct coap_packet reply;
@@ -106,7 +107,7 @@ end:
 	return 0;
 }
 
-
+/* used to send a simple coap PUT request to the broker (server) */
 static int send_simple_coap_request(const struct cloud_msg *const msg)
 {
 	uint8_t payload[strlen(msg->buf)];
@@ -130,6 +131,7 @@ static int send_simple_coap_request(const struct cloud_msg *const msg)
 		goto end;
 	}
 
+  // iterate over available broker paths, currently just 1, but could be extended
 	for (p = test_path; p && *p; p++) {
 		r = coap_packet_append_option(&request, COAP_OPTION_URI_PATH,
 					      *p, strlen(*p));
@@ -153,7 +155,6 @@ static int send_simple_coap_request(const struct cloud_msg *const msg)
 		LOG_ERR("Not able to append payload");
 		goto end;
 	}
-  LOG_INF("===== SENDING msg in 159");
 	r = send(sock, request.data, request.offset, 0);
 
 end:
@@ -162,6 +163,7 @@ end:
 	return 0;
 }
 
+/* helper to send data and recv the response, if any */
 static int send_simple_coap_msgs_and_wait_for_reply(const struct cloud_msg *const msg)
 {
 	int err;
@@ -173,13 +175,14 @@ static int send_simple_coap_msgs_and_wait_for_reply(const struct cloud_msg *cons
 
 	err = process_simple_coap_reply();
 	if (err < 0) {
-		// TODO cloud error message
 		LOG_INF("reply was parsed, err %d", err);
 		return err;
 	}
 	return 0;
 }
 
+/* sends observe requests to the broker, to the configured obs_paths
+ * currently obs paths are just /led, but could be extended in head of this file */
 static int send_obs_coap_request(void)
 {
 	struct coap_packet request;
@@ -193,7 +196,7 @@ static int send_obs_coap_request(void)
 	}
 
 	r = coap_packet_init(&request, data, APP_COAP_MAX_MSG_LEN,
-			     1, COAP_TYPE_CON, 8, coap_next_token(), // TODO changed to non_con from  CON
+			     1, COAP_TYPE_CON, 8, coap_next_token(),
 			     COAP_METHOD_GET, coap_next_id());
 	if (r < 0) {
 		LOG_ERR("Failed to init CoAP message");
@@ -214,7 +217,6 @@ static int send_obs_coap_request(void)
 			goto end;
 		}
 	}
-	LOG_INF("===== SENDING msg in 219");
 	r = send(poll_sock, request.data, request.offset, 0);
 
 end:
@@ -223,10 +225,12 @@ end:
 	return r;
 }
 
+/* processes messages on the poll_sock, reserved for observe events
+ * responds with ACKs if necessary and parses payload. Will create a cloud_notify_event
+ * if any payload was received
+ * This function is periodcally called by the observe() thread */
 static int process_obs_coap_reply(void)
 {
-
-	LOG_INF("processing OBS reply");
 	struct coap_packet reply;
 	int err;
 	uint16_t id;
@@ -245,18 +249,8 @@ static int process_obs_coap_reply(void)
 	}
 
 	LOG_INF("Waiting for LED patch");
-
-	// MSG_DONTWAIT would be non-blocking, but we want blocking
-	// as we need the data to update. nrf_recv should support this, but will only
-	// work with nrf_socket(), but this did not properly work.
-	// recv from zephyr does not support MSG_WAITALL, so we loop until we can read from socket
-	// rcvd = nrf_recv(poll_sock, data, APP_COAP_MAX_MSG_LEN, NRF_MSG_WAITALL);
-  rcvd = -1;
-	// while(rcvd < 0){
-	rcvd = recv(poll_sock, data, APP_COAP_MAX_MSG_LEN, 0); //MSG_DONTWAIT); TODO changed this to 0 from MSG_DONTWAIT
-	// }
-
-	LOG_INF("got after recv with msg length: %d", rcvd);
+  // recv blocks by default, will only act if a patch comes in
+	rcvd = recv(poll_sock, data, APP_COAP_MAX_MSG_LEN, 0);
 
 	if (rcvd == 0) {
 		ret = -EIO;
@@ -280,67 +274,33 @@ static int process_obs_coap_reply(void)
 		goto end;
 	}
 
-	LOG_INF("also after coap_packet_parse");
-
-	// tkl = coap_header_get_token(&reply, (uint8_t *)token);
-	// token_len = coap_header_get_token(&reply, token);
+  // parse message headers
 	token_len = coap_header_get_token(&reply, (uint8_t *)token);
-
-	// if ((token_len != sizeof(next_token)) &&
-	//     (memcmp(&next_token, token, sizeof(next_token)) != 0)) {
-	// 	printk("Invalid token received: 0x%02x%02x\n",
-	// 	       token[1], token[0]);
-	// 	// return 0;
-	// }
-
-
-	LOG_INF("parsed header_token");
 	id = coap_header_get_id(&reply);
-	LOG_INF("parsed coap_header_id");
 	type = coap_header_get_type(&reply);
-	LOG_INF("parsed coap_header_type");
 
-
-
-	LOG_INF("ID: %d", id);
-	LOG_INF("TYpe: %d", type);
-	LOG_INF("token: %s", token);
-
-	// send back ACK if received type con
+	// do nothing if ACK received, send back ACK if received type CON
 	if (type == COAP_TYPE_ACK) {
-		LOG_INF("received ACK");
 		ret = 0;
 		// in case of udpate, confirm update received from server
 	} else if (type == COAP_TYPE_CON) {
-		LOG_INF("recieved CON");
 		ret = send_obs_reply_ack(id, token, token_len);
 	} else {
-		// probably a RST, e.g. type 3
-		LOG_INF("ERROR, received RST");
-		ret = 0;
-	  // return ret;
+		// should never get here, this might only happen if server ran into problems
+		// with our request and returns RST or invalid message
+		LOG_ERR("ERROR, unexpected message received.");
+		return -1;
 	}
-
-
 
 	payload = coap_packet_get_payload(&reply, &payload_len);
 
+	LOG_INF("received message. payload:  %s", payload);
 
-
-	LOG_INF("GOT payload:  %s", payload);
-
-	// err = json_escape(payload, sizeof(payload), sizeof(payload));
-  // if(err < 0){
-	// 	LOG_INF("could not parse json");
-	// }
-	// LOG_INF("parsed payload: %s", payload);
-
-	// TODO replace test_data with payload.
-
+  // trigger cloud event with received data
 	struct cloud_event cloud_evt = {
 		.type = CLOUD_EVT_DATA_RECEIVED,
-		.data.msg.buf = payload, //&payload,
-		.data.msg.len = payload_len //payload
+		.data.msg.buf = payload,
+		.data.msg.len = payload_len
 	};
 	cloud_notify_event(coap_cloud_backend, &cloud_evt, payload);
 
@@ -350,7 +310,7 @@ end:
 	return ret;
 }
 
-/* stop observing */
+/* resets the observation to stop observing */
 static int send_obs_reset_coap_request(void)
 {
 	struct coap_packet request;
@@ -364,7 +324,7 @@ static int send_obs_reset_coap_request(void)
 	}
 
 	r = coap_packet_init(&request, data, APP_COAP_MAX_MSG_LEN,
-			     1, COAP_TYPE_CON, 8, coap_next_token(), // TODO changing from COAP_TYPE_CON to non confirmable
+			     1, COAP_TYPE_CON, 8, coap_next_token(),
 			     0, coap_next_id());
 	if (r < 0) {
 		LOG_ERR("Failed to init CoAP message");
@@ -413,15 +373,15 @@ static struct sockaddr_in parse_ip_address(int port, char *ip_address) {
   return addr;
 }
 
-
+/* initialize endpoints (broker) for the communication, setup sockets and connections */
 static int broker_init(void) {
 
 	int err;
 
 	char *ip_address = CONFIG_COAP_SERVER_HOSTNAME; // "83.150.54.152";
-	broker = parse_ip_address(5683, ip_address);
+	broker = parse_ip_address(5683, ip_address); // use standard coap port
 
-	// initialize socket for coap message sending
+	// initialize socket for coap message sending on UDP
 	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	 if (sock < 0) {
 	   LOG_ERR("Failed to create CoAP socket: %d.\n", errno);
@@ -436,7 +396,7 @@ static int broker_init(void) {
 	    return -errno;
 	  }
 
-  LOG_INF("Created broker sock: %d", sock);
+  LOG_DBG("Created broker socket: %d", sock);
 
   // init_coap_client
 	client.sock = sock;
@@ -444,7 +404,6 @@ static int broker_init(void) {
 
 
 	// create second socket for polling thread
-	// poll_sock = nrf_socket(NRF_AF_INET, NRF_SOCK_DGRAM, NRF_IPPROTO_UDP);
 	poll_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	 if (poll_sock < 0) {
 	   LOG_ERR("Failed to create CoAP socket: %d.", errno);
@@ -456,10 +415,9 @@ static int broker_init(void) {
 	   LOG_ERR("Connect for poll sock failed : %d", errno);
 	   return -errno;
 	 }
-	 LOG_INF("Created observe poll sock: %d", poll_sock);
+	 LOG_DBG("Created observe poll socket: %d", poll_sock);
 
 	// should send connected and ready events
-	LOG_INF("Cloud backend ready, triggering CONNECTED and READY");
 	struct cloud_backend_config *config = coap_cloud_backend->config;
 	struct cloud_event cloud_evt_connected = { CLOUD_EVT_CONNECTED };
 	struct cloud_event cloud_evt_ready = { CLOUD_EVT_READY };
@@ -475,55 +433,56 @@ static int broker_init(void) {
 #define POLL_THREAD_STACK_SIZE 16384
 #endif
 
+/* runs in the separate observe thread
+ * starts an observation request and then keeps calling "process_obs_coap_reply"
+ * to parse observation updates from the server */
 static void observe(void){
-
-	LOG_INF("Started observation thread");
+	LOG_DBG("Started observation thread");
 
 	int r;
 
-	/* wait for the thread semaphore being freed by the connection poll start */
+	/* wait for the thread semaphore being freed by the connection poll start
+	 * this will be done by the main application thread once the setup is complete */
 	k_sem_take(&connection_poll_sem, K_FOREVER);
 
-	LOG_INF("Was able to start the polling");
-
-	/* then start observing */
+	/* then start observing by sending an observation request */
 	r = send_obs_coap_request();
 	if (r < 0) {
-		// todo cloud error event
 		return r;
 	}
 
+  // keep parsing inputs until application stops
 	while (1) {
+		// if a disconnect was requested by the application
 		if(atomic_get(&disconnect_requested)){
 			LOG_INF("expected disconnect event");
-			return;
+			r = send_obs_reset_coap_request();
+				if(r < 0){
+					LOG_ERR("Failed to reset observe. Stopping listener anyway.");
+				}
+				k_sem_give(&connection_poll_sem);
+				(void)nrf_close(poll_sock);
+			return 0;
 		}
 
 		r = process_obs_coap_reply();
 		LOG_INF("RESULT OF process_obs_coap_reply: %d", r);
 		if (r < 0) {
-			// todo cloud error event
 			return r;
 		}
-
-		/* Unregister if stopped, and close socket afterwards */
-		// if (!atomic_get(&connection_poll_active)) {
-		// 	r = send_obs_reset_coap_request();
-		// 	if(r < 0){
-		// 		LOG_ERR("Failed to reset observe. Stopping listener anyway.");
-		// 	}
-		// 	k_sem_give(&connection_poll_sem);
-		// 	(void)nrf_close(poll_sock);
-		// }
 	}
 	return 0;
 }
 
-/* defines the observe thread to get updates from resource server */
+/* defines the observe thread to get updates from resource server,
+ * starts directly upon application start, is blocked by a semaphore until
+ * until main application thread is ready with the setup */
 K_THREAD_DEFINE(connection_poll_thread, POLL_THREAD_STACK_SIZE,
 		observe, NULL, NULL, NULL,
 		K_LOWEST_APPLICATION_THREAD_PRIO, 0, 0);
 
+/* starts the observe thread by giving the connection_poll_sem semaphore to
+ * the observation thread  */
 static int connection_poll_start(void)
 {
 	/* give semaphore to allow observe thread to start listening on poll sock */
@@ -532,73 +491,21 @@ static int connection_poll_start(void)
 	return 0;
 }
 
-static int connect_error_translate(const int err)
-{
-	switch (err) {
-#if defined(CONFIG_CLOUD_API)
-	case 0:
-		return CLOUD_CONNECT_RES_SUCCESS;
-	case -ECHILD:
-		return CLOUD_CONNECT_RES_ERR_NETWORK;
-	case -EACCES:
-		return CLOUD_CONNECT_RES_ERR_NOT_INITD;
-	case -ENOEXEC:
-		return CLOUD_CONNECT_RES_ERR_BACKEND;
-	case -EINVAL:
-		return CLOUD_CONNECT_RES_ERR_PRV_KEY;
-	case -EOPNOTSUPP:
-		return CLOUD_CONNECT_RES_ERR_CERT;
-	case -ECONNREFUSED:
-		return CLOUD_CONNECT_RES_ERR_CERT_MISC;
-	case -ETIMEDOUT:
-		return CLOUD_CONNECT_RES_ERR_TIMEOUT_NO_DATA;
-	case -ENOMEM:
-		return CLOUD_CONNECT_RES_ERR_NO_MEM;
-	case -EINPROGRESS:
-		return CLOUD_CONNECT_RES_ERR_ALREADY_CONNECTED;
-	default:
-		LOG_ERR("AWS IoT backend connect failed %d", err);
-		return CLOUD_CONNECT_RES_ERR_MISC;
-#else
-	case 0:
-		return COAP_CLOUD_CONNECT_RES_SUCCESS;
-	case -ECHILD:
-		return COAP_CLOUD_CONNECT_RES_ERR_NETWORK;
-	case -EACCES:
-		return COAP_CLOUD_CONNECT_RES_ERR_NOT_INITD;
-	case -ENOEXEC:
-		return COAP_CLOUD_CONNECT_RES_ERR_BACKEND;
-	case -EINVAL:
-		return COAP_CLOUD_CONNECT_RES_ERR_PRV_KEY;
-	case -EOPNOTSUPP:
-		return COAP_CLOUD_CONNECT_RES_ERR_CERT;
-	case -ECONNREFUSED:
-		return COAP_CLOUD_CONNECT_RES_ERR_CERT_MISC;
-	case -ETIMEDOUT:
-		return COAP_CLOUD_CONNECT_RES_ERR_TIMEOUT_NO_DATA;
-	case -ENOMEM:
-		return COAP_CLOUD_CONNECT_RES_ERR_NO_MEM;
-	case -EINPROGRESS:
-		return COAP_CLOUD_CONNECT_RES_ERR_ALREADY_CONNECTED;
-	default:
-		LOG_ERR("AWS broker connect failed %d", err);
-		return CLOUD_CONNECT_RES_ERR_MISC;
-#endif
-	}
-}
-
+/* not implemented, but required for the could api */
 int coap_cloud_keepalive_time_left(void)
 {
-	LOG_DBG("keepalive_time_left called, not expected.");
+	LOG_DBG("keepalive_time_left called, not implemented.");
 	return 5;
 }
 
+/* not implemented, but required for the could api */
 int coap_cloud_input(void)
 {
-	LOG_DBG("received data in cloud_input, not expected.");
+	LOG_DBG("received data in cloud_input, not implemented.");
 	return 0;
 }
 
+/* initializes the cloud connection */
 int coap_cloud_connect(void)
 {
 	int err;
@@ -609,11 +516,12 @@ int coap_cloud_connect(void)
 			LOG_ERR("client_broker_init, error: %d", err);
 			return err;
 		}
-		err = connect_error_translate(err);
+		// err = connect_error_translate(err);
 		err = connection_poll_start();
 	return err;
 }
 
+/* initializes the cloud backend */
 static int c_init(const struct cloud_backend *const backend,
 		  cloud_evt_handler_t handler)
 {
@@ -627,13 +535,6 @@ static int c_init(const struct cloud_backend *const backend,
 		.client_id_len = backend->config->id_len
 	};
   return 0;
-}
-
-static int c_ep_subscriptions_add(const struct cloud_backend *const backend,
-				  const struct cloud_endpoint *const list,
-				  size_t list_count)
-{
-	return 0;
 }
 
 static int c_connect(const struct cloud_backend *const backend)
@@ -651,24 +552,9 @@ static int c_disconnect(const struct cloud_backend *const backend)
 static int c_send(const struct cloud_backend *const backend,
 		  const struct cloud_msg *const msg)
 {
-	// LOG_INF("SENDING SENSOR DATA. MESSAGE: %s\n", msg->buf);
-	// TODO temporary disabled to test observe
 	int err;
 	err = send_simple_coap_msgs_and_wait_for_reply(msg);
-	return 0; //err;
-}
-
-/* called periodically to keep connection to broker alive
- * @return 0 if successful, error code otherwies */
-static int c_ping()
-{
-	LOG_DBG("c_ping called, this was not expected.");
-	return 0;
-}
-
-static int c_keepalive_time_left(const struct cloud_backend *const backend)
-{
-	return 5;
+	return err;
 }
 
 static const struct cloud_api coap_cloud_api = {
@@ -676,10 +562,7 @@ static const struct cloud_api coap_cloud_api = {
 	.connect		  = c_connect,
 	.disconnect		= c_disconnect,
 	.send		    	= c_send,
-	.ping		    	= c_ping,
-	.keepalive_time_left	= c_keepalive_time_left,
-	.input			  = coap_cloud_input,
-	.ep_subscriptions_add	= c_ep_subscriptions_add
+	.input			  = coap_cloud_input
 };
 
 
